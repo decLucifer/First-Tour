430badd17640dcfd605d36e185fac43184b79400
Abulfazl Shokriyan <shokriyanabulfazl@gmail.com>
2024-03-29 11:00:22 -0400

    REvome comments

---
+++ tutorial-mod-template-1.20.4/src/main/java/net/abulfazl/tutorialmod/TutorialMODClient.java
@@ -0,0 +1,10 @@
+package net.abulfazl.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialMODClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ tutorial-mod-template-1.20.4/src/main/java/net/abulfazl/tutorialmod/TutorialMODDataGenerator.java
@@ -0,0 +1,11 @@
+package net.abulfazl.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialMODDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
